9b0e59424f8aea8da700d3770ee03992efb0d61b
Leonardo Comelli <leonardo.comelli@gmail.com>
2019-05-21 20:24:50 -0300

    adds logging as json

--- main.go
+++ main.go
@@ -6,9 +6,13 @@ import (
 	"net/http"
 	"os"
 	"strings"
+
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
+
+	logrus.SetFormatter(&logrus.JSONFormatter{})
 
 	path := os.Getenv("DEFAULT_PATH")
 	if len(os.Args) > 1 {
@@ -19,9 +23,10 @@ func main() {
 		path = "/" + path
 	}
 
-	log.Printf("hello world in %s\n", path)
+	logrus.WithField("path", path).Info("starting simple-api...")
 
 	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		logrus.Infof("hello world in %s", path)
 		w.Write([]byte(fmt.Sprintf("hello world in %s", path)))
 	})
 
